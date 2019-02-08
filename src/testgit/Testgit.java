d6787f3ad22782facde9890b96a7550b54b023f2
unknown <Hillsoftgit@hillsoft.fr>
2019-02-08 16:07:58 +0100

    third test

---
+++ src/testgit/Testgit.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package testgit;
+
+/**
+ * @author Utilisateur
+ *
+ */
+public class Testgit {
+
+	/**
+	 * 
+	 */
+	public Testgit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("eclipe git");
+	}
+
+}
